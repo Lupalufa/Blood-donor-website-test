3840a42cb5b2a324294bd4c37635e05602d6edd2
Lupalufa <lupalufa431@gmail.com>
2024-09-15 18:27:42 -0300

    update: Automação de taxa de atualização autônoma

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+// Configurando o servidor //
 const express = require("express")
 const server = express()
 
-server.get("/", function(req, res){
-    return res.send("ok, cheguei aqui!")
+// Configurando a template engine //
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server
 })
 
+// configurando a apresentação da página // 
+server.get("/", function(req, res){
+    return res.render("index.html")
+})
+
+
+// Ligando o servidor e permiir o acesso na porta 3000 //
 server.listen(3000, function(){
     console.log("iniciei o servidor.")
 })
